6f76d70f148d9cec092f8077be403ebd8e38285e
przem <maz89prz@gmail.com>
2021-03-26 14:22:23 +0000

    removing redundant packages

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
-require('dotenv').config()
 
 const cakeRoutes = require('./routes/cake.routes')
 const photoRoutes = require('./routes/photo.routes')
